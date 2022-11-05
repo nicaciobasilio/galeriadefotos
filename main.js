8d1132766fdd5517355427e63167f96e003f5f58
Nicacio Basilio <nicacioufrn@gmail.com>
2022-11-05 13:39:47 -0300

    commit initial

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+    })
+})
